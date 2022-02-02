bf48ddf5ddffed15339d52851f18651679f51c34
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-02-02 15:06:59 -0300

    exercicio bloco 4 - dia 1

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-Primeiros-passos/exercicio-fixacao2/script.js
@@ -0,0 +1,12 @@
+
+const valor = prompt("QUal o valor de 1 a 100?")
+
+
+if (valor >=80){
+    console.log("Parabens, vc foi aprovado");
+}else if(valor <80 && valor >=60){
+    console.log("Você está na lista de espera");
+}else {
+    console.log("VocÊ foi reprovado")
+}
+
